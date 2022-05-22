ce1e205a7e677c46e44ce20ff29028d103451c2c
Arkadiusz Wojtasik <voytasic@aro.com>
2022-05-22 02:24:22 +0200

    modify circleci config

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React & Docker
         </a>
-        <p>App #2</p>
+        <p>App #3</p>
       </header>
     </div>
   );
